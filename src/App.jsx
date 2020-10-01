77ddd2ff3487cd12d6dedb9433fd99878ddc4cc4
kar-21 <karthiks210198@gmail.com>
2020-10-01 09:02:56 +0530

    adding welcome page & accept username

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import WelcomePage from "./components/Welcome";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
           <Route
             exact
             path="/"
-            component={() => <>Battle Ship</>}
+            component={() => <WelcomePage />}
           />
         </Switch>
       </BrowserRouter>
