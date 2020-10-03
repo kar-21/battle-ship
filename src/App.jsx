3fe16c770c7dec758d1b7be2cb50097647694a40
kar-21 <karthiks210198@gmail.com>
2020-10-03 16:24:55 +0530

    arrange ship feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
 import React, { useState } from "react";
+import { Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import WelcomePage from "./components/Welcome";
-import Error from "./components/Error";
+import "./App.scss";
+
+const Welcome = React.lazy(() => import("./components/Welcome"));
+const ArrangeShips = React.lazy(() => import("./components/ArrangeShip"));
+const Error = React.lazy(() => import("./components/Error"));
 
 const App = () => {
   let [userName, setUserName] = useState("");
+  let [gridArray, setGridArray] = useState([]);
 
   const getUserName = (name) => {
     setUserName(name);
-    console.debug('User Name App Component >>', userName);
+    console.debug("User Name App Component >>", userName);
+  };
+
+  const getGridArray = (gridArray) => {
+    setGridArray(gridArray);
   };
 
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={() => <WelcomePage getUserName={getUserName} />}
-          />
-          <Route path="/**" component={() => <Error />} />
-        </Switch>
-      </BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={() => <Welcome getUserName={getUserName} />}
+            />
+            <Route
+              exact
+              path="/arrangeShip"
+              component={() => (
+                <ArrangeShips userName={userName} getGridArray={getGridArray} />
+              )}
+            />
+            <Route
+              exact
+              path="/battleGround"
+              component={() => <div>{gridArray}</div>}
+            />
+            <Route path="/**" component={() => <Error />} />
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </>
   );
 };
